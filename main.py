1ffe959fffda8a16385fccf5f2f4aa3519131178
Iain Stewart <iainstewart08@gmail.com>
2024-02-20 18:20:02 +0000

    Dictionary / list comprehension practice

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+data = pandas.read_csv("./nato_phonetic_alphabet.csv")
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_name = input("Enter your name: ").upper()
+phonetic_translation = [data_dict[letter] for letter in user_name]
+
+print(phonetic_translation)
